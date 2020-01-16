306c521dce569d06982a7457a35e0588cd3b836d
Chineek Favors <chineekfavors01@gmail.com>
2020-01-15 21:01:30 -0800

    started get api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, (req, res) => {
+    console.log(`server listening on port 4000`);
+});
+
+server.get('/api/users')
